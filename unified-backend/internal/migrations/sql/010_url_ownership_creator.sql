8b72cfca8c8b52bd595d012c032b14e9a8b3b797
Никита Лучкин <48565773+nedoletoff@users.noreply.github.com>
2026-06-09 12:28:32 +0300

    feat(migrations): add permissions/roles/role_permissions tables, add role_id to users

--- unified-backend/internal/migrations/sql/010_url_ownership_creator.sql
+++
@@ -1,7 +0,0 @@
--- +migrate Up
-ALTER TABLE url_ownership
-    ADD COLUMN IF NOT EXISTS owner_username TEXT NOT NULL DEFAULT '';
-
--- +migrate Down
-ALTER TABLE url_ownership
-    DROP COLUMN IF EXISTS owner_username;
